3247266889a7ef2f9afe7fa59f0c04eae2ae9c06
nandukalidindi <nandukalidindi@gmail.com>
2017-06-11 13:51:58 +0530

    Add textbox sample with working code

--- example/src/example.js
+++ example/src/example.js
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom';
 import JsonifiedHtml from '../../src/JsonifiedHtml.js';
 
 class App extends React.Component {
-	render() {
+
+	textBoxHtmlJSON() {
+		return {
+		  "tag": "div",
+		  "attributes": { "style": { "width": "200px", "height": "50px" } },
+		  "children": [
+		    {
+		      "tag": "input",
+		      "attributes": {
+						"type": "text",
+						"defaultValue": "Enter Something",
+						"style": {
+							"width": "100%", "height": "100%", "border": "none"
+						}
+					},
+		    }
+		  ]
+		};
+	}
+
+	render() {	
 		return (
 			<div>
-				<JsonifiedHtml />
+				<JsonifiedHtml elementJSON={this.textBoxHtmlJSON()}/>
 			</div>
 		);
 	}
--- src/JsonifiedHtml.js
+++ src/JsonifiedHtml.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
-class JsonifiedHtml extends React.Component {
+class JsonifiedHtml extends React.Component {	
+
+	buildReactElement(jsonHTML) {
+    if(jsonHTML === null) return null;
+
+    var argumentList = [jsonHTML["tag"], jsonHTML["attributes"]];
+
+    Object.keys(jsonHTML).forEach(function(prop) {
+      if(["tag", "attributes", "event_listeners"].indexOf(prop) === -1)
+        jsonHTML["attributes"][prop] = jsonHTML[prop];
+    });
+
+    if(jsonHTML["event_listeners"]) {
+      Object.keys(jsonHTML["event_listeners"]).forEach(function(event_listener) {
+        jsonHTML["attributes"][event_listener] = this[jsonHTML["event_listeners"][event_listener]].bind(this)
+      }.bind(this));
+    }
+
+    (jsonHTML["children"] || []).forEach(function(child) {
+      argumentList.push(this.buildReactElement(child));
+    }.bind(this));
+
+    return React.createElement.apply(this, argumentList);
+  }
 
 	render() {
 		return (
 			<div>
-				WHAT THE HELL
+				{this.buildReactElement(this.props.elementJSON)}
 			</div>
 		);
 	}

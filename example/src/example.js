0ca7128ac3ea47aae7d768e94e8b8e4e5a8f569b
nandukalidindi <nandukalidindi@gmail.com>
2017-06-11 16:08:04 +0530

    Move the sample textbox html json to an object rather than a function

--- example/src/example.js
+++ example/src/example.js
@@ -4,29 +4,29 @@ import JsonifiedHtml from '../../src/JsonifiedHtml.js';
 
 class App extends React.Component {
 
-	textBoxHtmlJSON() {
-		return {
-		  "tag": "div",
-		  "attributes": { "style": { "width": "200px", "height": "50px" } },
-		  "children": [
+	constructor(props) {
+    super(props);
+    this.state = {};
+
+		this.textBoxHtmlJSON = {
+		  tag: 'div',
+		  attributes: { style: { width: '200px', height: '50px' } },
+		  children: [
 		    {
-		      "tag": "input",
-		      "attributes": {
-						"type": "text",
-						"defaultValue": "Enter Something",
-						"style": {
-							"width": "100%", "height": "100%", "border": "none"
-						}
+		      tag: 'input',
+		      attributes: {
+						type: 'text', defaultValue: 'Enter Something',
+						style: { width: '100%', height: '100%' }
 					},
 		    }
 		  ]
 		};
-	}
+  }
 
-	render() {	
+	render() {
 		return (
 			<div>
-				<JsonifiedHtml elementJSON={this.textBoxHtmlJSON()}/>
+				<JsonifiedHtml elementJSON={this.textBoxHtmlJSON}/>
 			</div>
 		);
 	}
